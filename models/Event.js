cd67552d01984f3ffc65b45d3e26b4269305eec3
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-09-25 12:14:12 +0800

    connect post method to front-end

--- models/Event.js
+++ models/Event.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+
 const eventSchema = new Schema({
 	start: Date,
 	end: Date,
--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,23 +1,23 @@
+const mongoose = require("mongoose");
+const Event = mongoose.model("events");
+
 module.exports = app => {
 	app.get("/events", (req, res) => {
-		res.send({
-			events: [
-				{
-					start: "Mon Sep 23 2019 16:30:30 GMT+0800",
-					end: "Fri Sep 28 2019 00:00:00 GMT+0800",
-					title: "Go to work"
-				},
-				{
-					start: "Sat Nov 16 2019 00:00:00 GMT+0800",
-					end: "Sun Jan 5 2020 00:00:00 GMT+0800",
-					title: "Holiday"
-				},
-				{
-					start: "Tue Sep 24 2019 18:30:00 GMT+0800",
-					end: "Tue Sep 24 2019 23:00:00 GMT+0800",
-					title: "Go Home"
-				}
-			]
+		Event.find({}, function(error, events) {
+			if (error) {
+				res.render("error", { errorMsg: "Problem!" });
+			}
+			res.send(events);
 		});
+	});
+
+	app.post("/", async (req, res) => {
+		const { start, end, title } = req.body;
+		const event = await new Event({
+			start: new Date(start),
+			end: new Date(end),
+			title
+		}).save();
+		res.send(req.body);
 	});
 };
